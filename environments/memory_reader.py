4944f5f234e1d32190acfd14702ea105d1170431
angrypark <sungnam1108@naver.com>
2018-06-25 10:20:01 +0900

    added environments and agents

---
+++ environments/memory_reader.py
@@ -0,0 +1,34 @@
+import win32api
+import win32process
+
+class MemoryReader:
+    PROCESS_ALL_ACCESS = 0x1F0FFF
+    
+    def __init__(self, handler, base_address):
+        self.handler = handler
+        self.pid = self.get_pid()
+        self.base_address = base_address
+        self.read_process_memory = ctypes.windll.kernel32.ReadProcessMemory
+        
+    def get_pid(self):
+        _, pid = win32process.GetWindowThreadProcessId(self.handler)
+        return pid
+    
+    def get_score(self):
+        buffer = ctypes.c_char_p(b"0")
+        
+        # read com score
+        self.read_process_memory(process_handle, base_addr+0x448f1d0, buffer, len(buffer.value), ctypes.byref(ctypes.c_ulong(0)))
+        com_score = int.from_bytes(buffer.value, byteorder='little')
+        
+        # read my score
+        self.read_process_memory(process_handle, base_addr+0x448f1d4, buffer, len(buffer.value), ctypes.byref(ctypes.c_ulong(0)))
+        my_score = int.from_bytes(buffer.value, byteorder='little')
+        
+        return com_score, my_score
+    
+    def is_over(self):
+        # read flag
+        self.read_process_memory(process_handle, base_addr+0x448f1d4+8, buffer, len(buffer.value), ctypes.byref(ctypes.c_ulong(0)))
+        flag = int.from_bytes(buffer.value, byteorder='little')
+        return flag
---
+++ environments/state.py
@@ -0,0 +1,46 @@
+import win32gui
+import win32ui
+from ctypes import windll
+from PIL import Image, ImageGrab
+import numpy as np
+
+from .memory_reader import MemoryReader
+
+class State:
+    def __init__(self, handler, config):
+        """
+        :param handler: 피카츄배구의 윈도우 핸들러 번호
+        :param config.image_size: 미리 이미지의 사이즈를 정할 수 있음
+        """
+        
+        self.handler = handler
+        left, top, right, bottom = win32gui.GetWindowRect(self.handler)
+        self.bbox = (left+10, top+50, right-5, bottom-20)
+        self.image_size = config.image_size
+        self.memory_reader = MemoryReader(self.handler, config.base_address)
+        
+    def get_state(self, gray=True):
+        """Get image array of state
+        :param gray: if true, get grayscale image (default True)
+        """
+        img = ImageGrab.grab(self.bbox)
+        img = img.resize(self.image_size, Image.ANTIALIAS)
+        if gray:
+            img = img.convert("L")
+        img_array = np.array(img, dtype=np.uint8)
+        return img_array
+    
+    def get_score(self):
+        com_score, my_score = self.memory_reader.get_score()
+        return com_score, my_score
+    
+    def get_image(self, resize=True, gray=False):
+        img = ImageGrab.grab(self.bbox)
+        if resize:
+            img = img.resize(self.image_size, Image.ANTIALIAS)
+        if gray:
+            img = img.convert("L")
+        return img
+    
+    def is_over(self):
+        return self.memory_reader.is_over()
